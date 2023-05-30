ca84d6c3e374b7ceb87c79374bca9ef5f50b2990
Nalyd59 <dy.crepin@gmail.com>
2023-05-30 17:03:14 +0200

    mise en place des formes

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 //let canvas = document.getElementById("canvas");
 //let ctx = canvas.getContext("2d");
+
+function myfonction(toto){
+  document.getElementById('change').setAttribute('href',toto);
+}
