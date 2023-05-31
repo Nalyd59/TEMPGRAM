2d88dc9687c316f9365fddeb73047186927b8a9a
Nalyd59 <dy.crepin@gmail.com>
2023-05-31 11:59:13 +0200

    modif fichier desordre

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Fonction pour remplacer le css
 function myfonction(toto){
   document.getElementById('change').setAttribute('href',toto);
 }
+
+//Ajoute le titre créé comme premier enfant de l'élément body
+let b = document.body;
+let titre = document.createElement('h1');
+
+titre.textContent = 'TEMPGRAM';
+
+b.prepend(titre);
