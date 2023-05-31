e73f9caa66919ed03a53b2e6a1c9b7e7502dd85d
Nalyd59 <dy.crepin@gmail.com>
2023-05-31 10:43:16 +0200

    ajout de figures

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//let canvas = document.getElementById("canvas");
-//let ctx = canvas.getContext("2d");
-
 function myfonction(toto){
   document.getElementById('change').setAttribute('href',toto);
 }
