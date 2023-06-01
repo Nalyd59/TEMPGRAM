f48c5977a82994631ec228eac0db545dcdb1262b
Nalyd59 <dy.crepin@gmail.com>
2023-06-01 16:28:58 +0200

    style

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Fonction pour remplacer le css
 function myfonction(toto){
   document.getElementById('stylepage').setAttribute('href',toto);
+  toto = toto.slice(9,16);
+  audio = new Audio('song/'+toto+'.mp3');
+  audio.duration = 2;
+  audio.play();
 }
 
 //Ajoute le titre créé comme premier enfant de l'élément body
